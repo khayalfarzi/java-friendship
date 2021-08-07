4184f632425f04509b8b8af3a752989693044b3f
khayalfarzi <xeyalferziyev9@gmail.com>
2021-08-07 11:27:14 +0300

    lesson 2: basic STDOUT examples

--- src/main/java/az/company/java_lessons/lesson_2/Main.java
+++ src/main/java/az/company/java_lessons/lesson_2/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello java!");
+        System.out.println("We are ready to learn java...");
+
+        String a = "Khayal";
+
+        System.out.printf("My name is %s", a);
     }
 }
