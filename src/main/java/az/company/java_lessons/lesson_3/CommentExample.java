fd430cfe97a73ced338d964a6c3c4771466747df
khayalfarzi <xeyalferziyev9@gmail.com>
2021-08-11 20:07:51 +0300

    lesson 3: explain JDK, JVM, JRE, JIT compiler and comments

---
+++ src/main/java/az/company/java_lessons/lesson_3/CommentExample.java
@@ -0,0 +1,28 @@
+package az.company.java_lessons.lesson_3;
+
+public class CommentExample {
+
+
+    public static void main(String[] args) {
+
+        int number = 5;
+        // This code mean is print value of the number
+        System.out.println(number);
+
+        //Print number between from 0 to 10 in the console
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        // one line comment
+
+        /*
+         * multi line comment
+         * */
+
+        /**
+         * javadoc documented comment
+         * @Author Khayal Farziyev
+         * */
+    }
+}
---
+++ src/main/java/az/company/java_lessons/lesson_3/Main.java
@@ -0,0 +1,19 @@
+package az.company.java_lessons.lesson_3;
+
+public class Main {
+
+    // there are a lot of code
+    public static void main(String[] args) {
+
+        int a = 6;  //     <- hvhvcckhahbcjklkckjsdbc ->  00001011100011011
+
+        if (a < 5)
+            System.out.println("kicik");
+        else System.out.println("boyuk");
+
+        System.out.println(a);
+    }
+
+    // there are a lot of code
+
+}
