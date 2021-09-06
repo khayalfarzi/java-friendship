b5b58677ef3d6f7c39979124659d3691cc9e09bd
khayalfarzi <xeyalferziyev9@gmail.com>
2021-09-06 20:01:00 +0300

    lesson 8: warmup

---
+++ src/main/java/az/company/java_lessons/lesson_8/warmup/SimpleAndComplexNumbers.java
@@ -0,0 +1,24 @@
+package az.company.java_lessons.lesson_8.warmup;
+
+import java.util.Scanner;
+
+public class SimpleAndComplexNumbers {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("==========> WELCOME TO THE APPLICATION ! <==========");
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        String result = "";
+
+        // write your code here
+        // if number is simple then result must be "SIMPLE"
+        // if number is complex result must be "COMPLEX"
+        // ....
+
+        System.out.println(result);
+        System.out.println("==========> Good bye ! <==========");
+    }
+}
