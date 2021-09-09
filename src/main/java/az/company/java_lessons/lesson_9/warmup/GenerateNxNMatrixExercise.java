0e0699ed96140ae18774cb0de8d43c8292a52b74
khayalfarzi <khayal.farziyev@pashabank.az>
2021-09-09 19:41:14 +0400

    lesson 9: warmup

---
+++ src/main/java/az/company/java_lessons/lesson_9/warmup/GenerateNxNMatrixExercise.java
@@ -0,0 +1,20 @@
+package az.company.java_lessons.lesson_9.warmup;
+
+public class GenerateNxNMatrixExercise {
+
+    /**
+     * WELCOME TO THE APPLICATION!
+     * Enter  row size: 3
+     * Enter column size: 3
+     * <p>
+     * Your NxN matrix is ready!
+     * <p>
+     * * * *
+     * * * *
+     * * * *
+     */
+    public static void main(String[] args) {
+
+        // todo: Write your code here!
+    }
+}
