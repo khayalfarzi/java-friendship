c90a06d1fadb75355333c75c80ddc1e694aaeedd
khayalfarzi <khayal.farziyev@pashabank.az>
2021-09-09 20:00:29 +0400

    lesson 9: warmup

--- src/main/java/az/company/java_lessons/lesson_9/warmup/GenerateNxNMatrixExercise.java
+++ src/main/java/az/company/java_lessons/lesson_9/warmup/GenerateNxNMatrixExercise.java
@@ -1,5 +1,7 @@
 package az.company.java_lessons.lesson_9.warmup;
 
+import java.util.Scanner;
+
 public class GenerateNxNMatrixExercise {
 
     /**
@@ -16,5 +18,25 @@ public class GenerateNxNMatrixExercise {
     public static void main(String[] args) {
 
         // todo: Write your code here!
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("WELCOME TO THE APPLICATION!");
+
+        // take row size
+        System.out.print("Enter row size: ");
+        int row = sc.nextInt();
+
+        // take column size
+        System.out.print("Enter columns size: ");
+        int columns = sc.nextInt();
+
+        // generate NxN matrix
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.printf("%s ", "*");
+            }
+            System.out.println();
+        }
     }
 }
